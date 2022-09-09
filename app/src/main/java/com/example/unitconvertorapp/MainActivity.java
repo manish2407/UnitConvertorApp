100a0f91d2981a8da5d05f7eb4c1f6fa999ea430
manish2407 <manishms2407@gmail.com>
2022-09-09 22:49:36 +0530

    Initial commit

---
+++ app/src/main/java/com/example/unitconvertorapp/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.unitconvertorapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+    private TextView textView;
+    private EditText text;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+        textView = findViewById(R.id.textView);
+        text = findViewById(R.id.text);
+    }
+}
